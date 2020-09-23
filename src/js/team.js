ce9f0c0966931909fe2bffeb95b84e4b966ceda4
tamithia <astrallynja@gmail.com>
2020-09-23 22:18:17 +0200

    Fix toggle hide on team member

--- src/js/team.js
+++ src/js/team.js
@@ -1,3 +1,11 @@
 import "../css/team.scss";
 import "./main";
 
+var $ = require("jquery");
+window.jQuery = $;
+window.$ = $;
+
+$(".card").click(function() {
+    $(".card").not(this).children(".info").slideUp();
+    $(".info", this).slideToggle();
+});
