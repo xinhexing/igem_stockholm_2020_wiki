38fc4693a743776c039b459ccadb43b1c9a2d2e4
tamithia <astrallynja@gmail.com>
2020-09-26 19:41:02 +0200

    Bug fixes

--- src/js/team.js
+++ src/js/team.js
@@ -4,12 +4,3 @@ import "./main";
 var $ = require("jquery");
 window.jQuery = $;
 window.$ = $;
-
-/*
-$(".card").click(function() {
-    $(".card").not(this).children(".info").hide();
-    $(".card").not(this).children(".avatar").show();
-    $(".info", this).fadeToggle();
-    $(".avatar", this).fadeToggle();
-});
-*/
